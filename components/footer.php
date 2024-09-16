9ae0626fd60fc86791f003b447b413d761c8a5f3
K.H.M.A.R.Riligala <137805971+AYODH@users.noreply.github.com>
2024-09-17 00:01:53 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<!--footer section starts-->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <a href="tel:1234567890"><i class="fas fa-phone"></i>+123-456-7890</a>
+            <a href="tel:1112223333"><i class="fas fa-phone"></i>+111-222-3333</a>
+            <a href="mailto:adminrochi@gmail.com"><i class="fas fa-envelope"></i>adminrochi@gmail.com</a>
+            <a href="mailto:rochirili@gmail.com"><i class="fas fa-envelope"></i>rochirili@gmail.com</a>
+            <a href="#"><i class="fas fa-map-marker-alt"></i> Colombo 0006, Sri Lanka</a>
+        </div>
+
+        <div class="box">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#reservation">reservation</a>
+            <a href="#gallery">gallery</a>
+            <a href="#contact">contact</a>
+            <a href="#reviews">reviews</a>
+        </div>
+
+        <div class="box">
+            <a href="#">Facebook <i class="fab fa-facebook-f"></i></a>
+            <a href="#">Twitter <i class="fab fa-twitter"></i></a>
+            <a href="#">Instagram <i class="fab fa-instagram"></i></a>
+            <a href="#">LinkedIn <i class="fab fa-linkedin"></i></a>
+        </div>
+
+    </div>
+    <div class="credit">&copy;Copyright @ 2024 by Rochi | All rights reserved!</div>
+
+</section>
+<!--footer section ends-->
